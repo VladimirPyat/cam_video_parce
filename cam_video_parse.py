775f3fec7bb615e6d7768e463d47ad9e4b150bb3
Vladimir Pyat <scared@mail.ru>
2024-06-20 02:43:58 +0300

    add zones

--- cam_video_parse.py
+++ cam_video_parse.py
@@ -1,6 +1,7 @@
 import os
 import time
 
+
 def format_time(seconds):
     m, s = divmod(seconds, 60)
     h, m = divmod(m, 60)
@@ -32,13 +33,25 @@ SOURCE_DIR = '_source'
 RESULT_DIR = '_result'
 
 CAM_AREAS = {
-    '248': (0.1, 0.45, 0.55, 0.25)
+    '248': (0.15, 0.45, 0.55, 0.25),
+    '249': (0.25, 0.0, 0.45, 0.35),
+    '252': (0.35, 0.4, 0.45, 0.2)
 }  # ключ - номер камеры. значения - кортеж координат области детекции
+# x_percent, y_percent, w_percent, h_percent
 
 
 CLASSES_TO_DETECT = [2, 3, 7]  # ищем машины, мотоциклы, грузовики
 
-FRAMES_SKIP = 6  # пропуск части кадров в видео для ускорения работы
+FRAMES_SKIP = 4  # пропуск части кадров в видео для ускорения работы
+
+
+def get_cam_id(filename):
+    parts = filename.split('.')
+    parts = parts[-2].split('_')
+    cam_num = parts[0]
+    cam_numbers = list(CAM_AREAS.keys())
+
+    return cam_num if cam_num in cam_numbers else cam_numbers[0]
 
 
 def is_path_exist():
@@ -110,20 +123,24 @@ def video_proceed(video_filename, cam_id=tuple(CAM_AREAS.keys())[0], show_img=Fa
         frame_counter += 1
         # Обработка кадра каждые FRAMES_SKIP кадров
         if frame_counter % FRAMES_SKIP == 0:
-            frame_crop = image_crop(frame, cam_id)
-            result = yolo_obj_detect(frame_crop)[0]
+            try:
+                frame_crop = image_crop(frame, cam_id)
+                result = yolo_obj_detect(frame_crop)[0]
 
-            if is_obj_detected(result) and is_obj_motion(result):
-                detection_counter += 1
-                frame_filename = f"{video_filename}_{frame_counter}.jpg"
-                frame_path = os.path.join(CURRENT_DIR, RESULT_DIR, frame_filename)
-                if show_img:
-                    cv2.imshow('Frame', frame_crop)
+                if is_obj_detected(result) and is_obj_motion(result):
+                    detection_counter += 1
+                    frame_filename = f"{video_filename}_{frame_counter}.jpg"
+                    frame_path = os.path.join(CURRENT_DIR, RESULT_DIR, frame_filename)
+                    if show_img:
+                        cv2.imshow('Frame', frame_crop)
 
-                if cv2.waitKey(1) & 0xFF == ord('q'):
-                    break
+                    if cv2.waitKey(1) & 0xFF == ord('q'):
+                        break
 
-                cv2.imwrite(frame_path, frame)  # Сохранение кадра если обнаружен объект
+                    cv2.imwrite(frame_path, frame)  # Сохранение кадра если обнаружен объект
+            except Exception as e:
+                print(f'Произошла ошибка при обработке кадра {frame_counter}: {str(e)}')
+                continue  # Пропуск битого кадра и продолжение выполнения
 
     cap.release()
     cv2.destroyAllWindows()
@@ -131,7 +148,7 @@ def video_proceed(video_filename, cam_id=tuple(CAM_AREAS.keys())[0], show_img=Fa
     print(f'Сохранено {detection_counter} файлов в {RESULT_DIR}')
 
 
-def video_path_proceed(cam_id=tuple(CAM_AREAS.keys())[0], show_img=False, files_extention='mp4'):
+def video_path_proceed(show_img=False, files_extention='mp4'):
     video_path = os.path.join(CURRENT_DIR, SOURCE_DIR)
     if not os.path.exists(video_path):
         print(f'Путь {video_path} к обрабатываемым файлам не найден')
@@ -140,17 +157,16 @@ def video_path_proceed(cam_id=tuple(CAM_AREAS.keys())[0], show_img=False, files_
     files_list = [file for file in os.listdir(video_path) if file.endswith(files_extention)]
 
     for file in files_list:
-        video_proceed(file, cam_id=cam_id, show_img=show_img)
-
-
-
-
-
+        video_proceed(file, cam_id=get_cam_id(file), show_img=show_img)
 
 
 if __name__ == '__main__':
+    # для тестирования рекомендуется попробовать на небольшом фрагменте.
+    # раскомментировать следующие две строки, ввести имя файла
 
-    # video_file_name = 'test.mp4'
-    # video_proceed(video_file_name)
+    # video_file_name = '10.121.15.252_01_20240527062249177_4-.mp4'
+    # video_proceed(video_file_name, cam_id=get_cam_id(video_file_name), show_img=True)
+
+    # следующую строку раскомментировать когда уже всё настроено. для обработки всех файлов в папке
 
     video_path_proceed()
